1ccbf5b96cee827f704b13fbf3abcc4d51824c75
olegkuprianov <76089731+olegkuprianov@users.noreply.github.com>
2020-12-16 09:13:30 +0300

    Add files via upload

---
+++ server_test_simple.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 16:00:46 2020
+
+@author: web
+
+server folders
+
+test_py/test_1
+
+"""
+
+phrase = 'Hello! Test_1'
+
+with open("text.txt", "w") as text_file:
+    text_file.write(phrase)
+    
