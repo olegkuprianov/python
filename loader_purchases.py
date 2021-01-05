c22376dbac2ca3737cbae52e6c071d3c1153d64e
olegkuprianov <76089731+olegkuprianov@users.noreply.github.com>
2021-01-06 00:10:02 +0300

    Update loader_purchases.py

--- loader_purchases.py
+++ loader_purchases.py
@@ -47,7 +47,7 @@ db_connection = mysql.connector.connect(
 )
 mycursor = db_connection.cursor()
 
-yesterday = datetime.today() - timedelta(days=2)
+yesterday = datetime.today() - timedelta(days=3)
 yesterday_ymd = yesterday.strftime('%Y-%m-%d')
 
 mycursor.execute('SELECT * FROM events WHERE event_name = "order_paid" AND event_datetime = ("' + yesterday_ymd + '") ORDER BY event_datetime')
